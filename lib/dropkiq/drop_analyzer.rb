726babd5d81270a1ee9c401c446d863c2e042a34
Adam Darrah <piremies@gmail.com>
2019-11-11 17:05:14 -0500

    Easy case tests for column methods

--- lib/dropkiq/drop_analyzer.rb
+++ lib/dropkiq/drop_analyzer.rb
@@ -1,10 +1,13 @@
 # Assumes that Drop class is named ModelNameDrop
 # Assumes that Drop class has one argument that takes an instance
+# Assumes that Drop class method will return same type if has the same name
 
 module Dropkiq
   class DropAnalyzer
     attr_accessor :liquid_drop_class,
-      :active_record_class
+      :active_record_class,
+      :drop_methods,
+      :table_name
 
     def initialize(liquid_drop_class)
       self.liquid_drop_class = liquid_drop_class
@@ -12,6 +15,8 @@ module Dropkiq
 
     def analyze
       self.active_record_class = find_active_record_class
+      self.table_name = active_record_class.table_name
+      self.drop_methods = find_drop_methods
     end
 
     private
@@ -23,6 +28,18 @@ module Dropkiq
 
       namespaces.push(non_drop).join("::").constantize
     rescue NameError
+    end
+
+    def find_drop_methods
+      default_methods  = (Liquid::Drop.instance_methods + Object.instance_methods)
+      instance_methods = (liquid_drop_class.instance_methods - default_methods)
+
+      instance_methods.map do |method|
+        {
+          type: active_record_class.columns_hash[method.to_s].type,
+          name: method
+        }
+      end
     end
   end
 end
--- test/dropkiq/drop_analyzer_test.rb
+++ test/dropkiq/drop_analyzer_test.rb
@@ -9,6 +9,18 @@ class DropkiqDropAnalyzerTest < Minitest::Test
     def name
       @person["name"]
     end
+
+    def age
+      @person["age"]
+    end
+
+    def active
+      @person["active"]
+    end
+
+    def created_at
+      @person["created_at"]
+    end
   end
 
   class Person < ActiveRecord::Base
@@ -24,6 +36,9 @@ class DropkiqDropAnalyzerTest < Minitest::Test
       age: 34
     })
     @person_drop = PersonDrop.new(@person)
+
+    @analyzer = Dropkiq::DropAnalyzer.new(PersonDrop)
+    @analyzer.analyze
   end
 
   def teardown
@@ -31,9 +46,35 @@ class DropkiqDropAnalyzerTest < Minitest::Test
   end
 
   def test_finds_correct_active_record_model
-    @analyzer = Dropkiq::DropAnalyzer.new(PersonDrop)
-    @analyzer.analyze
-
     assert_equal Person, @analyzer.active_record_class
+  end
+
+  def test_finds_correct_table_name
+    assert_equal Person.table_name, @analyzer.table_name
+  end
+
+  def test_correctly_identifies_string_column
+    @column = @analyzer.drop_methods.detect{|data| data[:name] == :name}
+    assert_equal :string, @column[:type]
+  end
+
+  def test_correctly_identifies_integer_column
+    @column = @analyzer.drop_methods.detect{|data| data[:name] == :age}
+    assert_equal :integer, @column[:type]
+  end
+
+  def test_correctly_identifies_boolean_column
+    @column = @analyzer.drop_methods.detect{|data| data[:name] == :active}
+    assert_equal :boolean, @column[:type]
+  end
+
+  def test_correctly_identifies_datetime_column
+    @column = @analyzer.drop_methods.detect{|data| data[:name] == :created_at}
+    assert_equal :datetime, @column[:type]
+  end
+
+  def test_columns_not_implemented_in_drop_class_are_hidden
+    @column = @analyzer.drop_methods.detect{|data| data[:name] == :updated_at}
+    assert_nil @column
   end
 end
