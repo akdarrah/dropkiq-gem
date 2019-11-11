2741aaa8c207a667195b8253fd6e7239b971a270
Adam Darrah <piremies@gmail.com>
2019-11-11 13:29:34 -0500

    first commit

---
+++ lib/dropkiq.rb
@@ -0,0 +1,6 @@
+require "dropkiq/version"
+
+module Dropkiq
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/dropkiq_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class DropkiqTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Dropkiq::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
