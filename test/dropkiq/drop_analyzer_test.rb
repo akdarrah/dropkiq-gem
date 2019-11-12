860d0a968290211d66cbd1d65bc8d94f1f2a8ec3
Adam Darrah <piremies@gmail.com>
2019-11-11 23:17:56 -0500

    Test data to play with

--- test/dropkiq/drop_analyzer_test.rb
+++ test/dropkiq/drop_analyzer_test.rb
@@ -60,8 +60,15 @@ class DropkiqDropAnalyzerTest < Minitest::Test
       notes: "A banana is an edible fruit – botanically a berry – produced by several kinds of large herbaceous flowering plants in the genus Musa. In some countries, bananas used for cooking may be called \"plantains\", distinguishing them from dessert bananas. Wikipedia",
       age: 34
     })
+
+    @group = Group.create!(name: "Chiquita Banana", owner: @person)
+    @group.people << @person
+
     @banana_tag = Tag.create!({name: "Banana"})
     @orange_tag = Tag.create!({name: "Orange"})
+
+    @person.tags << @banana_tag
+    @person.tags << @orange_tag
 
     @person_drop = PersonDrop.new(@person)
 
